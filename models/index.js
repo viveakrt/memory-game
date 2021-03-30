c1879a173705e701d8dce839cf5caaa883be5a6d
Vivek <vivek.15@mountblue.tech>
2021-03-30 18:00:10 +0530

    Bug fixed added config files

--- models/index.js
+++ models/index.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config');
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+sequelize = new Sequelize(config.database, config.username, config.password,{
+  host: config.host,
+  dialect: 'mysql',
 }
+);
 
 fs
   .readdirSync(__dirname)
